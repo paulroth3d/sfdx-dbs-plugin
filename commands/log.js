a1d2ee905d2ebfa6eb5d6b79714233b80a399f63
Paul Roth <proth@salesforce.com>
2017-09-11 20:58:05 -0500

    UPDATE: complete initial implementation of log.

--- commands/log.js
+++ commands/log.js
@@ -1,6 +1,8 @@
 /*esversion: 6*/
 const fs = require('fs-extra');
 const path = require('path');
+const LineByLineReader = require('line-by-line');
+const Q=require('q');
 
 /**
  *  Module that works with the latest logs of sfdx CLI.
@@ -25,6 +27,130 @@ const path = require('path');
     return( results );
   }
   
+  /**
+   * Determines the last line of the log.
+   * @param logPath
+   * @return Promise (lastLineOfFile:String)
+   **/
+  function getLastLineOfFile(logPath){
+    let deferred=Q.defer();
+    
+    fs.pathExists(logPath)
+      .then( function(exists){
+        //console.log('exists');
+        //console.log('logPath:' + logPath);
+        
+        let reader = new LineByLineReader(logPath);
+        let lastFunctionalLine='';
+        
+        reader.on('error', function(err){
+          deferred.reject('Error occurred while reading file',err);
+        });
+        
+        reader.on('line', function(line){
+          //-- skip over empty lines
+          if( line ){
+            lastFunctionalLine=line;
+          }
+        });
+        
+        reader.on('end', function(){
+          //console.log('end of file');
+          //console.log('last functional line');
+          //console.log(lastFunctionalLine);
+          deferred.resolve(lastFunctionalLine);
+        });
+      })['catch']( function( err, exists){
+        deferred.reject('Error caught before reading file', err);
+      });
+    
+    return(deferred.promise);
+  }
+
+  /**
+   * Determines the JSON context from the file provided
+   * @param lastLineFromFile (String)
+   * @return Object - parsed object
+   **/
+  function getContextObject(lastLineFromFile){
+    let deferred=Q.defer();
+    try {
+      let contextObj=JSON.parse(lastLineFromFile);
+      if(!contextObj){
+        deferred.reject('Unable to parse the last log line', {logLine:lastLineFromFile});
+      }
+      deferred.resolve(contextObj);
+    } catch(err){
+      deferred.reject('Error occurred while parsing last log line', {err:err, logLine:lastLineFromFile});
+    }
+    return(deferred.promise);
+  }
+  
+  /**
+   *  Attempts to get the message from the response
+   *  @param context (Object) - dx log context
+   *  @return (Object) - the parsed msg context
+  **/
+  function getMessageContext(context){
+    let deferred=Q.defer();
+    let msg;
+    
+    if( !context ){
+      deferred.reject('Empty context found',context);
+      return(deferred.promise);
+    } else if( !context.msg ){
+      deferred.reject('Log does not have msg',context);
+      return(deferred.promise);
+    }
+    
+    //console.log('found context.msg');
+    
+    //-- JSON does not support single quotes
+    let fixedMsg=context.msg.replace(/"/g, '\\"').replace(/'/g, '"');
+    let msgArray;
+    try {
+      msgArray=JSON.parse(fixedMsg);
+      if(!msgArray){
+        deferred.reject('unable to parse msg', msg);
+        return(deferred.promise);
+      }
+      
+      //console.log('found msg');
+      //console.log(JSON.stringify(msgArray,null,2));
+    } catch(err){
+      deferred.reject('error occurred while parsing the log msg',fixedMsg);
+    }
+    
+    //console.log('found msgArray');
+    
+    let msgStr2;
+    for( var i = 0; i < msgArray.length; i++ ){
+      if(typeof msgArray[i] == 'string' ){
+        msgStr2=msgArray[i];
+        break;
+      }
+    }
+    
+    //console.log('found msgStr2');
+    
+    let finalMsg;
+    try {
+      finalMsg=JSON.parse(msgStr2);
+      if(!finalMsg){
+        deferred.reject('Unable to parse msg array',msgStr2);
+        return(deferred.promise);
+      }
+    } catch(err){
+      deferred.reject('Error parsing the msg array',msgStr2);
+    }
+    
+    //console.log('found everything');
+    //console.log(JSON.stringify(finalMsg,null,2));
+    deferred.resolve({context:context, msg:finalMsg});
+    
+    return(deferred.promise);
+  }
+  
   module.exports = {
     topic: 'log',
     command: 'latest',
@@ -37,19 +163,39 @@ const path = require('path');
       hasValue: true
     }],
     run(context){
-      console.log( 'ran the command' );
+      //console.log( 'ran the command' );
       //console.log( JSON.stringify(context,null,2) );
       
       let homeLog=getHomeLog(context);
-      console.log('homeLog=' + homeLog);
-      fs.pathExists(homeLog)
-        .then( function(exists){
-          console.log('exists');
-          console.log(exists);
-          console.log(arguments);
-        })['catch']( function( err, exists){
-          console.log( 'catch' );
-          console.log( JSON.stringify(arguments));
+      //console.log('homeLog=' + homeLog);
+      
+      getLastLineOfFile(homeLog)
+        .then(function(lastLineOfFile){
+          //console.log('success:'+lastLineOfFile);
+          return(getContextObject(lastLineOfFile));
+        })
+        .then(function(contextObj){
+          //console.log('found context object');
+          //console.log(JSON.stringify(contextObj,null,2));
+          return(getMessageContext(contextObj));
+        })
+        .then(function(results){
+          console.log('\n\n' +
+            'When running as:' + results.context.hostname + '\n' +
+            'at ' + results.context.time + '\n' +
+            'the following logs were found:\n'
+          );
+          console.log(JSON.stringify(results.msg,null,2));
+          //console.log('from here');
+          //console.log(JSON.stringify(results,null,2));
+          //console.log('found msg context');
+          //console.log(JSON.stringify(results.context,null,2));
+          //console.log('msgContext');
+          //console.log(JSON.stringify(results.msg,null,2));
+
+        }).catch( function(msg,errObj){
+          console.error('Error:' + msg );
+          console.error(JSON.stringify(errObj,null,2));
         });
     }
   };

63bbb8de4b0d628f3a6a15186c9fb60a5d5e6b0f
Paul Roth <proth@salesforce.com>
2017-09-20 05:53:49 +0300

    STUB: add in framework for compressing.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const logLatest = require('./commands/log/latest.js');
+const logLatest = require('./commands/log/latest');
+const compressDir = require('./commands/compress/dir');
 
 (function () {
   'use strict';
@@ -11,9 +12,13 @@ const logLatest = require('./commands/log/latest.js');
   exports.topics = [{
     name:'log',
     description: 'Reports on the latest cli JSON log',
+  }, {
+    name: 'compress',
+    description: 'Compressing functions'
   }];
   
   exports.commands = [
-    logLatest
+    logLatest,
+    compressDir
   ];
 }());
---
+++ test/compress-dir-spec.js
@@ -0,0 +1,16 @@
+/*eslint quotes: ['error', 'single']*/
+/*jshint esversion: 6, quotmark: single */
+
+let _ = require('underscore');
+let logModule = require('../commands/compress/dir');
+let assert = require('assert');
+let fs = require('fs-extra');
+  
+
+describe('Compress/Dir', function(){
+  
+  it('runs tests', function(){
+    assert.equal(true, true, 'Compress test yet to be created');
+  });
+
+});

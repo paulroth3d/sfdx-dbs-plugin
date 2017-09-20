fc55217c5c2c5fcf49006f0870804f03d15ebb2d
Paul Roth <proth@salesforce.com>
2017-09-20 05:27:21 +0300

    CLEANUP: move commands to appropriate directories
    Commands should be held in folders to follow the structure on how they run.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const log = require('./commands/log.js');
+const logLatest = require('./commands/log/latest.js');
 
 (function () {
   'use strict';
-  
-  exports.topics = [{
-    name:'log',
-    description: 'Reports on the latest cli JSON log',
-  }];
   
   exports.namespace = {
     name: 'dbs',
     description: 'A collection of SalesForce DX CLI plugins by Paul Roth'
   };
   
+  exports.topics = [{
+    name:'log',
+    description: 'Reports on the latest cli JSON log',
+  }];
+  
   exports.commands = [
-    log
+    logLatest
   ];
 }());

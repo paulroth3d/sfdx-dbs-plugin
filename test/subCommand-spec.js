f13ec6d194484f3ffe9c041d2afd83e77c88bbe3
Paul Roth <proth@salesforce.com>
2017-09-23 01:53:56 +0300

    UPDATE: include an example subcommand

---
+++ test/subCommand-spec.js
@@ -0,0 +1,15 @@
+var subCommandModule = require('../commands/example/example_subCommand');
+var assert = require('assert');
+
+describe('Sub Command runs', function (){
+  it('opens with a fake topic', function(done){
+    try {
+      subCommandModule.run({});
+      assert.equal(true,true,'No exceptions should have been called.');
+      done();
+    } catch(err){
+      assert.fail('exception should not be thrown');
+      done();
+    }
+  });
+});

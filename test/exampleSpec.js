28d665923897b69244407881ff70c251d2649ac3
Paul Roth <proth@salesforce.com>
2017-09-22 20:27:38 +0300

    UPDATE: add in an example:sayHello command.

--- test/exampleSpec.js
+++
@@ -1,9 +0,0 @@
-var helloWorld = require('../commands/helloWorld');
-var assert = require('assert');
-
-
-describe('Hello World', function (){
-  it('says hello', function (){
-    assert.equal(helloWorld(), 'Hello world!');
-  });
-});
